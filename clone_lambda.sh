45b7ab1a557d04b938ca6c7e10281a7165921e60
DevDooly <sunhongyi@gmail.com>
2020-08-10 15:05:49 +0900

    git clone all lambda

---
+++ clone_lambda.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+git clone https://github.com/bogonets/answer-lambda-av
+git clone https://github.com/bogonets/answer-lambda-condition
+git clone https://github.com/bogonets/answer-lambda-cv2
+git clone https://github.com/bogonets/answer-lambda-cv2-helper
+git clone https://github.com/bogonets/answer-lambda-detectron2
+git clone https://github.com/bogonets/answer-lambda-falldown
+git clone https://github.com/bogonets/answer-lambda-fire
+git clone https://github.com/bogonets/answer-lambda-headpose
+git clone https://github.com/bogonets/answer-lambda-image
+git clone https://github.com/bogonets/answer-lambda-intersect
+git clone https://github.com/bogonets/answer-lambda-logical
+git clone https://github.com/bogonets/answer-lambda-measure
+git clone https://github.com/bogonets/answer-lambda-minio
+git clone https://github.com/bogonets/answer-lambda-number
+git clone https://github.com/bogonets/answer-lambda-numpy
+git clone https://github.com/bogonets/answer-lambda-pose
+git clone https://github.com/bogonets/answer-lambda-preview
+git clone https://github.com/bogonets/answer-lambda-pytorch
+git clone https://github.com/bogonets/answer-lambda-rtc
+git clone https://github.com/bogonets/answer-lambda-string
+git clone https://github.com/bogonets/answer-lambda-tesseract
+git clone https://github.com/bogonets/answer-lambda-time
+git clone https://github.com/bogonets/answer-lambda-yolov4-tf
